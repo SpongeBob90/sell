cc4f6174e39ad68613aeb1334e2d384f5d3ce57c
SpongeBob90 <1442772080@qq.com>
2018-09-04 13:15:02 +0800

    跟视频学习-打包

---
+++ prod.server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+router.get('/',function (req,res,next) {
+  req.url = '/index.html';
+  next();
+});
+
+app.use(router);
+
+const appData = require('./data.json');
+const seller = appData.seller;
+const goods = appData.goods;
+const ratings = appData.ratings;
+
+router.get('/api/seller', function (req, res) {
+  res.json({
+    errno: 0,
+    data: seller
+  });
+});
+
+router.get('/api/goods', function (req, res) {
+  res.json({
+    errno: 0,
+    data: goods
+  });
+});
+
+router.get('/api/ratings', function (req, res) {
+  res.json({
+    errno: 0,
+    data: ratings
+  });
+});
+
+app.use('/api', router);
+
+app.use(express.static('./dist'));
+
+const config = require('./config/index');
+const port = process.env.PORT || config.build.port;
+module.exports = app.listen(port, function (err) {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log('Listening at http://localhost:'+port+'\n')
+})
+
